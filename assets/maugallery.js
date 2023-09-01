a01b5ef84de33f2f3b74a7d10763e7b281d67a1d
Pierre Schnell <pierreschnell@hotmail.com>
2023-09-01 12:52:59 +0200

    Ajout des srcset et sizes combiné à différente photo, correction des bug present et mise en page titre etc

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||         //Modification du code, enlève 1 à l'index pour naviguer vers l'image précédente
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];  //Modification du code: ajout + 1 à l'index afin de montrer l'image suivante
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
